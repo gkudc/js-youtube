e0375f7bbe22943a823ed2a7e0bd3c157fac98cb
gkudc <155797631+gkudc@users.noreply.github.com>
2024-05-18 16:12:45 +0000

    Arrays studied

---
+++ 02_basics/01_array.js
@@ -0,0 +1,17 @@
+const myArr = [3,5,2,1,7]
+//console.log(myArr)
+
+myArr.push(10);
+//console.log(myArr)
+
+const newArr = myArr.join()
+//console.log(myArr)
+//console.log(typeof newArr)
+
+// slice, splice
+
+console.log("A ", myArr)
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr)
